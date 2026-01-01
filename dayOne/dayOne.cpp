51458d12e63007c98246ab6b4ae287b5515b574c
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2026-01-01 11:37:22 -0700

    First pass coding. Haven't compiled

--- dayOne/dayOne.cpp
+++ dayOne/dayOne.cpp
@@ -1,14 +1,48 @@
 //Advent of code day one
 // lock picking, check for land on zero
 
-string fileLine;
+#include <iostream>
+using namespace std;
 
-ifstream MyReadFile("dayOne.txt");
+int main(){
+  string fileLine;
 
-while (getline (MyReadFile, fileLine)) {
-  char direction = fileLine[0];
-  try:
-    int clicks = (int) fileLine[1,fileLine.length()];
-  catch:
-    cout << "Impropper input formatting";
+  // dial starts at 50 and goes left and right from there.
+  int dial = 50;
+  /*int dial[100];
+
+  for (int i = 0; i<100; i++){
+    dial[i] = i;
+  }*/
+
+  int answer = 0;
+
+  ifstream MyReadFile("dayOne.txt");
+
+  //Read line by line, claims to drop the new line.
+  while (getline (MyReadFile, fileLine)) {
+    //Left or Right
+    char direction = fileLine.substr(0,1);
+    try{
+      //should be a single number to end of line
+      int clicks = (int) fileLine.substr(1);
+    }catch(){
+      cout << "Impropper input formatting";
+    }
+
+    // spin the dial
+    if(direction == 'L'){
+      dial = dial - clicks;
+    }else if(direction == 'R'){
+      dial = dial + clicks;
+    }else{
+      cout << "Impropper directional input.";
+    }
+
+    //bounding as the dial goes from 0-99
+    while(dial < 0)dial = dial + 100;
+    while(dial > 99)dial = dial - 100;
+    if(dial == 0)answer++;
+
+  }
 }
