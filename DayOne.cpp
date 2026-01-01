64b352591a6c0751ceaa15a0e7b7ddaab44b348d
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2025-12-31 21:56:39 -0700

    Create DayOne.cpp

---
+++ DayOne.cpp
@@ -0,0 +1,10 @@
+//Advent of code day one
+// lock picking, check for land on zero
+
+string fileLine;
+
+ifstream MyReadFile(".txt");
+
+while (getline (MyReadFile, myText)) {
+  cout << myText;
+}
