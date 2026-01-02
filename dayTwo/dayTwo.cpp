c161a61e7b272f6b15de140a510cefca9efd5bc6
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2026-01-01 21:10:35 -0700

    2nd day of Advent of Code Part 1 done

--- dayTwo/dayTwo.cpp
+++ dayTwo/dayTwo.cpp
@@ -10,30 +10,33 @@ int main(){
   string fileLine;
 
 
-  int answer = 0;
+  long long answer = 0;
 
   ifstream MyReadFile("input.txt");
 
   //Read line by line, claims to drop the new line.
   while (getline (MyReadFile, fileLine, ',')) {
-    stringstream ss(fileLine);
-    int val = 0;
-    int max = 0;
-    string s;
-    getline(ss, s,'-');
-    val = stoi(s);
-    getline(ss, s,'-');
-    max = stoi(s);
+    long long val = 0;
+    long long max = 0;
+
+    int l = fileLine.find('-');
+
+    val = stoll(fileLine.substr(0,l));
+    //cout << val << endl;
+    max = stoll(fileLine.substr(l+1));
+    //cout << max << endl;
 
     while (val<=max){
       string check = std::to_string(val);
       int len = check.length() >> 1;
       if((len << 1) == check.length()){
-        if(check.substr(0,len) == check.substr(len))answer++;
+        if(check.substr(0,len) == check.substr(len))answer += val;
       }
       val++;
     }
+    //cout << "Answer: ";
+    //cout << answer << endl;
   }
-  cout << answer;
-  return answer;
+  cout << answer << endl;
+  return 0;
 }
