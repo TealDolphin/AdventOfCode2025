15be26c31b07e3db2f71e0d3c9abc457113afffd
TealDolphin <44395321+TealDolphin@users.noreply.github.com>
2026-01-05 22:06:42 -0700

    I'm close to part 2

--- dayFive/dayFivePart2.cpp
+++ dayFive/dayFivePart2.cpp
@@ -31,25 +31,58 @@ int main(){
 
   //Read line by line, claims to drop the new line.
   while (getline (MyReadFile, fileLine)) {
-    l1++;
+    //l1++;
     //int len = fileLine.length();
     //const char *bank = fileLine.c_str();
     if(fileLine == "")break;
 
     int l = fileLine.find('-');
 
-    Node* n = new Node;
-    n->from = stol(fileLine.substr(0,l));
-    n->to = stol(fileLine.substr(l+1));
+    long from = stol(fileLine.substr(0,l));
+    long to = stol(fileLine.substr(l+1));
 
-    tail->next = n;
-    tail = tail->next;
+    Node* m = head->next;
+    while(m != nullptr){
+      if((from >= m->from) && (from <= m->to)){
+        if(to > m->to){
+          m->to = to;
+        }
+        from = 0;
+        to = 0;
+        break;
+      }else if((to >= m->from) && (to <= m->to)){
+        if(from < m->from){
+          m->from = from;
+        }
+        from = 0;
+        to = 0;
+        break;
+      }
+
+      m = m->next;
+    }
+    if(from != 0 && to != 0){
+      Node* n = new Node;
+      n->from = from;
+      n->to = to;
+
+      tail->next = n;
+      tail = tail->next;
+    }
   }
 
   Node* first = head;
   head = head->next;
   delete first;
 
+  Node* n = head;
+
+  while(n!=nullptr){
+    answer += (n->to - n->from);
+    n = n->next;
+  }
+
+  /*
   // Read in the values to query for the listed IDs
   while (getline (MyReadFile, fileLine)) {
     l2++;
@@ -62,9 +95,9 @@ int main(){
       }
       n = n->next;
     }
-  }
+  }*/
 
-  cout << l1 << "<>" << l2 << endl;
+  //cout << l1 << "<>" << l2 << endl;
 
   cout << answer << endl;
   return 0;
